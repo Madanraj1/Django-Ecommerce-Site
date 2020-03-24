51ddf1524c3a74f028d5d205e092fa61e9a9cf84
madanraj <madanraj251999@gmail.com>
2020-03-24 13:22:41 +0530

    Django-Ecommerce-site

---
+++ orders/forms.py
@@ -0,0 +1,9 @@
+from django import forms 
+from .models import Order 
+from localflavor.in_.forms import INZipCodeField
+
+class OrderCreateForm(forms.ModelForm):
+    postal_code = INZipCodeField()
+    class Meta:
+        model = Order 
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city'] 
